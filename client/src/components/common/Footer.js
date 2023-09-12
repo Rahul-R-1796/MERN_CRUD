621c7b74a8ff71c6bc2df97b6fe24733c1b52713
Rahul R <127472805+Rahul-R-1796@users.noreply.github.com>
2023-09-12 15:28:38 +0530

    Update Footer.js

--- client/src/components/common/Footer.js
+++ client/src/components/common/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
 	const link = "https://google.com";
 	const target = "_blank";
 
-	return (
+	return ( 
 		<div className="container">
 			Copyright © <small>{new Date().getFullYear()}</small> Rahul R{" "}
 			<a href={link} target={target}></a>
