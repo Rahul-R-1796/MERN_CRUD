d44506bc3b68c98ac7fe9f2ea26bbe9f5787bcbe
Rahul R <127472805+Rahul-R-1796@users.noreply.github.com>
2023-09-13 08:40:26 +0530

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+mongoose.connect('mongodb+srv://rahulrajenderan96:rahulrajenderan96@cluster0.xrqprey.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const Contact = mongoose.model('Contact', {
+  name: String,
+  mobileNo: String,
+  email: String,
+  address: String,
+});
+
+app.get('/api/contacts', async (req, res) => {
+  try {
+    const contacts = await Contact.find();
+    res.json(contacts);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+app.post('/api/contacts', async (req, res) => {
+  try {
+    const contact = new Contact(req.body);
+    await contact.save();
+    res.status(201).json(contact);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+app.put('/api/contacts/:id', async (req, res) => {
+  try {
+    const contact = await Contact.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.json(contact);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+app.delete('/api/contacts/:id', async (req, res) => {
+  try {
+    await Contact.findByIdAndRemove(req.params.id);
+    res.sendStatus(204);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
